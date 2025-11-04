cb33efa71451375395f7054dff15975799ff5758
benzovvozh <augusthoffman3@gmail.com>
2025-11-04 15:42:45 +0500

    init commit

---
+++ src/test/java/io/project/wolfgym/WolfGymApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.wolfgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WolfGymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
