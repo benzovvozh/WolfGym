797533c65a483b21265c94d6c072d4e6f4d6dc66
benzovvozh <augusthoffman3@gmail.com>
2025-11-06 22:35:59 +0500

    Переработаны тесты в интеграционные, добавлена миграция на создание записей шаблонов и записей промежуточной таблицы

---
+++ src/test/java/io/project/wolfgym/controller/ExerciseControllerIT.java
@@ -0,0 +1,116 @@
+package io.project.wolfgym.controller;
+
+import io.project.wolfgym.dto.exercise.ExerciseCreateDTO;
+import io.project.wolfgym.dto.exercise.ExerciseDTO;
+import io.project.wolfgym.model.MuscleGroup;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc(printOnlyOnFailure = false)
+class ExerciseControllerIT {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    private final String validExercise = """
+            {
+              "name": "Жим лежа",
+              "description": "Базовое упражнение для грудных мышц", 
+              "muscleGroup": "CHEST",
+              "videoUrl": "https://www.youtube.com/watch?v=example1"
+            }""";
+
+    private final String invalidExercise = """
+            {
+              "name": "Жим лежа", 
+              "description": "Базовое упражнение для грудных мышц",
+              "muscleGroup": "CHESTs",
+              "videoUrl": "https://www.youtube.com/watch?v=example1"
+            }""";
+
+
+    @Test
+    void createExercise_WithValidData_ReturnsOk() throws Exception {
+
+        mockMvc.perform(post("/api/exercises")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(validExercise))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("Жим лежа"))
+                .andExpect(jsonPath("$.muscleGroup").value("CHEST"))
+                .andExpect(jsonPath("$.description")
+                        .value("Базовое упражнение для грудных мышц"));
+
+    }
+
+    @Test
+    void createExercise_WithInvalidMuscleGroup_ReturnsBadRequest() throws Exception {
+        // When & Then
+        mockMvc.perform(post("/api/exercises")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(invalidExercise))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createExercise_WithMissingName_ReturnsBadRequest() throws Exception {
+        String invalidExerciseWithMissingName = """
+                {
+                  "description": "Базовое упражнение для грудных мышц",
+                  "muscleGroup": "CHEST"
+                }""";
+        mockMvc.perform(post("/api/exercises")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(invalidExerciseWithMissingName))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void showExercise_ReturnsOk() throws Exception {
+        mockMvc.perform(get("/api/exercises/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Жим лёжа"))
+                .andExpect(jsonPath("$.muscleGroup").value("CHEST"))
+                .andExpect(jsonPath("$.description").value("4 подхода по 10 раз"));
+    }
+
+    @Test
+    void showAllExercises_ReturnsOk() throws Exception {
+        //Then
+        mockMvc.perform(get("/api/exercises"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2)) // Проверяем количество элементов
+                .andExpect(jsonPath("$[0].name").value("Жим лёжа"))
+                .andExpect(jsonPath("$[0].muscleGroup").value("CHEST"))
+                .andExpect(jsonPath("$[0].description").value("4 подхода по 10 раз"))
+                .andExpect(jsonPath("$[1].name").value("Жим Арнольда"))
+                .andExpect(jsonPath("$[1].muscleGroup").value("SHOULDERS"))
+                .andExpect(jsonPath("$[1].description").value("Поднятие гантелей над головой"));
+    }
+
+    @Test
+    void deleteExercise_ReturnsNoContent() throws Exception {
+        mockMvc.perform(delete("/api/exercises/1"))
+                .andExpect(status().isNoContent());
+//        mockMvc.perform(get("/api/exercises/1"))
+//                .andExpect(status().isNotFound());
+    }
+
+}
---
+++ src/test/java/io/project/wolfgym/controller/WorkoutTemplateControllerIT.java
@@ -0,0 +1,113 @@
+package io.project.wolfgym.controller;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc(printOnlyOnFailure = false)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class WorkoutTemplateControllerIT {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    private final String validRequest = """
+            {
+              "name": "Спина + Бицепс",
+              "description": "Силовая тренировка для верхней части тела",
+              "exercisesIds": [1, 2]
+            }
+            """;
+    private final String invalidRequest = """
+            {
+              "name": null,
+              "description": "Силовая тренировка для верхней части тела",
+              "exercisesIds": [1]
+            }
+            """;
+
+    @Test
+    @Order(1)
+    void createWorkoutTemplate_withInvalidFields_ReturnsBadRequest() throws Exception {
+        //then
+        mockMvc.perform(post("/api/workout-templates")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(invalidRequest))
+                .andExpect(status().isBadRequest());
+
+    }
+
+    @Test
+    @Order(2)
+    void showWorkoutTemplateById_ReturnsOk() throws Exception {
+
+        mockMvc.perform(get("/api/workout-templates/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Грудные мышцы"))
+                .andExpect(jsonPath("$.description")
+                        .value("Тренировка грудной группы мышц"))
+                .andExpect(jsonPath("$.exercises").isArray())
+                .andExpect(jsonPath("$.exercises[0].name").value("Жим лёжа"))
+                .andExpect(jsonPath("$.exercises[0].description")
+                        .value("4 подхода по 10 раз"));
+
+    }
+
+    @Test
+    @Order(3)
+    void showAllWorkoutTemplates_ReturnsOk() throws Exception {
+
+        mockMvc.perform(get("/api/workout-templates"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].name").value("Грудные мышцы"))
+                .andExpect(jsonPath("$[0].description").value("Тренировка грудной группы мышц"))
+                .andExpect(jsonPath("$[0].exercises").isArray())
+                .andExpect(jsonPath("$[0].exercises").isNotEmpty())
+                .andExpect(jsonPath("$[0].exercises[0].name").value("Жим лёжа"))
+                .andExpect(jsonPath("$[1].name").value("Плечи"))
+                .andExpect(jsonPath("$[1].description").value("Тренировка плеч"))
+                .andExpect(jsonPath("$[1].exercises").isArray())
+                .andExpect(jsonPath("$[1].exercises").isNotEmpty())
+                .andExpect(jsonPath("$[1].exercises[0].name").value("Жим Арнольда"));
+    }
+
+    @Test
+    @Order(4)
+    void createWorkoutTemplate_withValidFields_ReturnsOk() throws Exception {
+        //Then
+        mockMvc.perform(post("/api/workout-templates")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(validRequest))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value("Спина + Бицепс"))
+                .andExpect(jsonPath("$.description")
+                        .value("Силовая тренировка для верхней части тела"))
+                .andExpect(jsonPath("$.exercises").isArray())
+                .andExpect(jsonPath("$.exercises.length()").value(2))
+                .andExpect(jsonPath("$.exercises[0].name").value("Жим лёжа"))
+                .andExpect(jsonPath("$.exercises[1].name").value("Жим Арнольда"));
+
+    }
+
+    @Test
+    @Order(5)
+    void deleteWorkoutTemplate_ReturnsNoContent() throws Exception {
+        mockMvc.perform(delete("/api/workout-templates/1"))
+                .andExpect(status().isNoContent());
+//        mockMvc.perform(get("/api/workout-templates/1"))
+//                .andExpect(status().isNotFound());
+    }
+
+}
